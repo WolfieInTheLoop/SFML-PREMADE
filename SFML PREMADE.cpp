590f59d1bf8c5063436ebb6afa6a200fe70c2dfc
Exequiel <104279077+DocBlackWolf@users.noreply.github.com>
2023-10-31 18:20:15 -0300

    added SFML locally

--- SFML PREMADE.cpp
+++ SFML PREMADE.cpp
@@ -1,19 +1,30 @@
-// SFML PREMADE.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+//Esto es una plantilla de SFML el codigo siguiente te permite verificar su correcto funcionamiento
 
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    // Create a window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test");
+
+    // Create a circle shape
+    sf::CircleShape circle(50);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(375, 275);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(); // Clear the window
+
+        // Draw the circle
+        window.draw(circle);
+
+        window.display(); // Display the contents of the window
+    }
+
+    return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
